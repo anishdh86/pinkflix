c6378060f2d62d6c61b45163cba8aae512174817
Anish Dhingra <webdesigner.anish86@gmail.com>
2021-06-28 22:30:54 +0200

    -  Updated routing and app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ApplicationFrameComponent } from './shared/components/application-frame/application-frame.component';
+import { AuthorizationAPIKeyInterceptor } from './shared/interceptor/authorize.interceptor';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ApplicationFrameComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/home'
+      },
+      {
+        path: 'home',
+        loadChildren: ()=> import('./home/home.module').then(
+          module => module.HomeModule
+        )
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthorizationAPIKeyInterceptor }
+  ]
 })
 export class AppRoutingModule { }
