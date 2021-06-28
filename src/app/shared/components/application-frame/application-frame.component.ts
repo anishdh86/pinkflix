88f5338df9807b20b45396b290434dd2e5f251f4
Anish Dhingra <webdesigner.anish86@gmail.com>
2021-06-28 22:31:51 +0200

    -  Added application frame component -  Added home module -  Added interceptor

---
+++ src/app/shared/components/application-frame/application-frame.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-frame',
+  templateUrl: './application-frame.component.html',
+  styleUrls: ['./application-frame.component.scss']
+})
+export class ApplicationFrameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/shared/interceptor/authorize.interceptor.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+@Injectable()
+export class AuthorizationAPIKeyInterceptor implements HttpInterceptor {
+
+  private readonly API_KEY: string = '3ce028a9843027900e410c7cfc19264d';
+  
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request.clone({
+      setHeaders: {
+        api_key: this.API_KEY
+      }
+    }))
+  }
+}
